36f48ac39f3e4bc94bb491dc274bfdf349793eb3
Mat Savage <mathewsavage@gmail.com>
2016-01-14 13:21:27 -0800

    Added null support, fixed initial onChangeFeature

--- Install/PythonEditCounter_addin.py
+++ Install/PythonEditCounter_addin.py
@@ -8,7 +8,10 @@ def UpdateLayerText(lyr):
     symbology = lyr.symbology
     valueList = [str(row[0]) for row in arcpy.da.SearchCursor(lyr,symbology.valueField)]
     for value in symbology.classValues:
-        valueDict[value] = valueList.count(value)
+        if value != "<Null>":
+            valueDict[value] = valueList.count(value)
+        else:
+            valueDict[value] = valueList.count('None')
     newLabels = []
     for i,v in enumerate(symbology.classValues):
         curLabel = re.sub(r'\([^)]*\)', '', symbology.classLabels[i]).strip()
@@ -16,6 +19,18 @@ def UpdateLayerText(lyr):
         newLabels.append(classLabel)
     lyr.symbology.classLabels = newLabels
     arcpy.RefreshTOC()
+
+def GetCurrentLayer(geometryObj,oidList,layerList):
+    layer = None
+    for layer in layerList:
+        if layer.isFeatureLayer:
+            oidField = arcpy.Describe(layer).OIDFieldName
+            query = "{0} in {1}".format(oidField,str(oidList).replace('[','(').replace(']',')'))
+            with arcpy.da.SearchCursor(layer,[oidField,"SHAPE@"],where_clause=query) as rows:
+                for row in rows:
+                    geom = row[1]
+                    if geom.JSON == geometryObj.JSON:
+                        return layer
 
 class PythonEditCounter(object):
     """Implementation for PythonEditCounter_addin.extension2 (Extension)"""
@@ -36,5 +51,22 @@ class PythonEditCounter(object):
 
     def onChangeFeature(self):
         lyr = self.currentLayer
-        if lyr.symbologyType == 'UNIQUE_VALUES':
-            UpdateLayerText(lyr)
+        if lyr is not None:
+            if lyr.symbologyType == 'UNIQUE_VALUES':
+                UpdateLayerText(lyr)
+        else:
+            #print self.editWorkspace
+            curFeat = self.currentFeature
+            curSel = self.editSelection
+            mxd = arcpy.mapping.MapDocument("CURRENT")
+            layers = arcpy.mapping.ListLayers(mxd)
+            #print curFeat,curSel
+            lyr = GetCurrentLayer(curFeat,curSel,layers)
+            if lyr is not None:
+                #print "Layer Found"
+                if lyr.symbologyType == 'UNIQUE_VALUES':
+                    UpdateLayerText(lyr)
+            #else:
+            #    print "Layer not found"
+
+            
